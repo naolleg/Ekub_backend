b50a6b3a01c9bca058e2f240beae8aa8f0e25422
naolleg <naollegesse51@gmail.com>
2024-06-03 14:02:37 +0300

    some mistake corrected

--- distt/src/api/deposit/depositRoute.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import errorHandler from "../../middlewares/error.js";
-import depositController from "./depositController.js";
-const depositRouter = Router();
-depositRouter.post('/', errorHandler(depositController.register));
-depositRouter.put('/:id', errorHandler(depositController.update));
-depositRouter.delete('/:id', errorHandler(depositController.delete));
-export default depositRouter;
--- distt/src/api/winner/winnerSchema.js
+++
@@ -1,7 +0,0 @@
-import { z } from 'zod';
-export const winnerSchema = {
-    register: z.object({
-        lotId: z.number().int().positive(),
-        registeredBy: z.number().int().positive(),
-    }),
-};
